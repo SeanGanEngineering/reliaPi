7d32132d1ffc9f966817665d2ac4107aa6491f84
Sean <sean@Seans-MacBook-Pro.local>
2023-10-03 21:25:04 +1100

    add rest api

--- reliaPi_bff/main.go
+++ reliaPi_bff/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	name := "Sean"
-	age := 26
-	favNumbers := []int{5, 4, 3}
-	favNumbers = append(favNumbers, 12)
-	vertices := make(map[string]int)
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
-	fmt.Println(favNumbers)
-	fmt.Println(name, age)
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	go router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
 }
