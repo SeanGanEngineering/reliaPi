053fca239427f05166871a866dfee50d6bcdbc71
Sean <sean@Seans-MacBook-Pro.local>
2023-10-06 21:11:55 +1100

    connected to db

--- bff/main.go
+++ bff/main.go
@@ -1,8 +1,24 @@
 // main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reliaPi/cmd/reliaPi"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	db, err := reliaPi.ConnectDB()
+	if err != nil {
+		fmt.Println("Error connecting to db", err)
+		return
+	}
+	defer db.Close()
+
+	if err := reliaPi.CreateDatabaseAndTables(db); err != nil {
+		fmt.Println("Error creating database and tables:", err)
+		return
+	}
+
+	fmt.Println("Database actions completed")
 }
