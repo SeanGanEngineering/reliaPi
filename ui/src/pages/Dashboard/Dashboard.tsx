1823f90a59e133b0ebcb4aec4f2c5a8c9f29d601
Sean <sean@Seans-MacBook-Pro.local>
2023-10-08 17:28:34 +1100

    get data from postgres

--- ui/src/pages/Dashboard/Dashboard.tsx
+++ ui/src/pages/Dashboard/Dashboard.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { PageContainer } from './Dashboard.styled';
 import LineGraph from '../../components/graph/LineGraph';
 import TemperatureAdd from '../../components/TemperatureAdd/TemperatureAdd';
+import { LineChart } from '@mui/x-charts';
 
 const Dashboard = () => {
+
+
   return (
     <PageContainer>
       <LineGraph />
