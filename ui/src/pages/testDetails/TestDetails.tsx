0b7a9c5343a13d86022c796b0ce00db639fa49a4
Sean <sean@Seans-MacBook-Pro.local>
2023-10-23 20:05:03 +1100

    add rhf

--- ui/src/pages/testDetails/TestDetails.tsx
+++ ui/src/pages/testDetails/TestDetails.tsx
@@ -4,39 +4,56 @@ import {
   NavWrapper,
   PaddingWrapper,
   PageWrapper,
-  StyledTextField,
-  TextInput,
 } from './TestDetails.styled';
-import { Autocomplete, TextField, Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { testCategories, testOptions } from './TestDetailsFixtures';
-import { DateTimePicker } from '@mui/x-date-pickers';
+import {
+  AutocompleteElement,
+  DateTimePickerElement,
+  FormContainer,
+  TextFieldElement,
+} from 'react-hook-form-mui';
 
 const TestDetails = () => {
   return (
     <PageWrapper>
-      <PaddingWrapper>
-        <Typography variant='h3'>Plan your test</Typography>
-        <br />
-        <TextInput label='Title' variant='outlined' />
-        <StyledTextField label='Description' multiline maxRows={4} />
-        <TextInput label='Assigned' variant='outlined' />
-        <DateTimeWrapper>
-          <DateTimePicker label='Start time' />
-          <DateTimePicker label='End time' />
-        </DateTimeWrapper>
-        <Autocomplete
-          options={testOptions}
-          renderInput={(params) => <TextField {...params} label='Metric' />}
-        />
-        <Autocomplete
-          options={testCategories}
-          renderInput={(params) => <TextField {...params} label='Test Type' />}
-        />
-        <br />
-        <NavWrapper>
-          <Next />
-        </NavWrapper>
-      </PaddingWrapper>
+      <FormContainer onSuccess={(data) => console.log(data)}>
+        <PaddingWrapper>
+          <Typography variant='h3'>Plan your test</Typography>
+          <br />
+          <TextFieldElement name='title' label='Title' />
+          <TextFieldElement
+            name='description'
+            label='Description'
+            multiline
+            maxRows={4}
+          />
+          <TextFieldElement
+            name='assigned'
+            label='Assigned'
+            variant='outlined'
+          />
+          <DateTimeWrapper>
+            <DateTimePickerElement name='startTime' label='Start time' />
+            <DateTimePickerElement name='endTime' label='End time' />
+          </DateTimeWrapper>
+          <AutocompleteElement
+            name='metric'
+            label='Metric'
+            options={testOptions}
+          />
+          <AutocompleteElement
+            name='testType'
+            label='testType'
+            options={testCategories}
+          />
+          <br />
+          <NavWrapper>
+            <Button type='submit'>Submit</Button>
+            <Next />
+          </NavWrapper>
+        </PaddingWrapper>
+      </FormContainer>
     </PageWrapper>
   );
 };
